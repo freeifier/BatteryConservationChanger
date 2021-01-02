35a53cb53a180dc4dc671053c06e4cc9f514eab6
freeifier <76787075+freeifier@users.noreply.github.com>
2021-01-02 10:19:35 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-echo "Type in your local user"
+echo "Type in your local user account:"
 read username
 path=$(pwd)
 
+echo "Copying programm files to: $path/BatteryConservationChanger"
 cp -r $path"/BatteryConservationChanger" "/home/$username/BatteryConservationChanger"
+
+sleep 1s
+echo "Changing user rights"
+chown -R $username:$username "/home/$username/BatteryConservationChanger"
+chmod -R 700 "/home/$username/BatteryConservationChanger"
+
+sleep 1s
+echo "Adding crontab entry"
 echo "@reboot root sleep 15s; chmod 646 /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode;" >> /etc/crontab
-echo "@reboot $username /bin/python3.8 /home/"$username"/BatteryConservationChanger/tray_icon.py" >> /etc/crontab
+
+sleep 1s
+echo "Creating auto-start menu entry"
+echo "[Desktop Entry]
+      Name=BatteryConservationChanger
+      Exec=/bin/python3.8 /home/$username/BatteryConservationChanger/tray_icon.py
+      Type=Application
+      Comment=BatteryConservationChanger" > /home/$username/.config/autostart/battery-conservation-changer.desktop
+
+echo "fixing ownership of autostart entry"
+chown -R $username:$username "/home/$username/.config/autostart/battery-conservation-changer.desktop"
