e58cc6c99752d1ec781965c4d37e9cac344ab183
freeifier <76787075+freeifier@users.noreply.github.com>
2021-01-02 09:20:05 +0100

    Add files via upload

--- tray_icon.py
+++ tray_icon.py
@@ -4,7 +4,7 @@ from gi.repository import Gtk, AppIndicator3 as appindicator
 #import Haupt_Window
 
 def main():
-    indicator = appindicator.Indicator.new("customtray",os.path.abspath('/home/andreas/Dokumente/BatteryConservationChanger/BattChangerIcon.png'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator = appindicator.Indicator.new("customtray",os.path.abspath('BattChangerIcon.png'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(menu())
     Gtk.main()
@@ -12,22 +12,18 @@ def main():
 def menu():
     menu = Gtk.Menu()
 
-    #haupt = Gtk.MenuItem('Open')
-    #haupt.connect('activate', open_haupt)
-    #menu.append(haupt)
-
     command_sw=Gtk.CheckMenuItem("Battery conservation mode")
     command_sw.set_active(get_current_state())
     command_sw.connect("activate",switch_state)
     menu.append(command_sw)
-    
+
     exittray = Gtk.MenuItem('Exit')
     exittray.connect('activate', quit)
     menu.append(exittray)
-    
+
     menu.show_all()
     return menu
-  
+
 
 def get_current_state()->str:
     state=open("/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode","r")
@@ -48,8 +44,6 @@ def switch_state(_):
     else:
         f.write("1")
         f.close()
-#def open_haupt(_):
-#    Haupt_Window.Bat_Main()
 
 if __name__ == "__main__":
     main()
